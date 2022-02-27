e4542e064ad1669ecbc88e9f6fe68b3c05ba4128
Gustavo780904 <gustavodutrasantos@gmail.com>
2022-02-27 18:19:23 -0300

    criptografia

--- criptografia1/src/criptografia1/Criptografia1.java
+++ criptografia1/src/criptografia1/Criptografia1.java
@@ -6,41 +6,55 @@ import java.util.Scanner;
 
 public class Criptografia1 {
 
-	public static void main(String[] args) {
+	static char[] senha = { 10, 8, 4, 11, 2 };
 
-		Locale.setDefault(Locale.US);
-		Scanner s = new Scanner(System.in);
-		
-		char[] senha = {10, 8, 4, 11, 2};
-
-		String mensagem = s.nextLine();
-		
-		char[] caracteres = mensagem.toCharArray();
-
-		
+	// alfabeto
+	public static ArrayList<String> alfabeto() {
 		ArrayList<String> alfabeto = new ArrayList<String>();
-		
+
 		alfabeto.add(" ");
-		for (int i = 65; i < 90; i++) {
+		for (int i = 65; i < 91; i++) {
+
 			char a = (char) i;
 			alfabeto.add(String.valueOf(a));
 		}
 		for (int i = 97; i < 123; i++) {
+
 			char a = (char) i;
 			alfabeto.add(String.valueOf(a));
 		}
-		
-		for (int i=0; i<mensagem.length();i++) {
-			for (int j=0; j<5; j++) {
-				caracteres[i]+=senha[j];
+
+		return alfabeto;
+
+	}
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner s = new Scanner(System.in);
+
+		String mensagem = s.nextLine();
+
+		//converte a mensagem (string) em caracteres
+		char[] caracteres = mensagem.toCharArray();
+
+		int tamanhoMsg = mensagem.length();
+		int diferenca = tamanhoMsg % 5;
+		if (diferenca != 0 || diferenca != 5) {
+			tamanhoMsg =+ tamanhoMsg - diferenca;
+		}
+			
+		for (int i = 0; i < tamanhoMsg; i++) {
+
+			for (int j = 0; j < 5; j++) {
+				caracteres[i] += senha[j];
 				i++;
 			}
+			i++;
 		}
-				
 
-		System.out.println(mensagem);
+
 		System.out.println(caracteres);
-		System.out.println(alfabeto);
 
 		s.close();
 	}
