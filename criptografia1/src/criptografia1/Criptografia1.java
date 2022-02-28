434c794c269b97fb722ea5b839ac232b689d57d0
Gustavo780904 <gustavodutrasantos@gmail.com>
2022-02-28 00:33:48 -0300

    soma da senha ok

--- criptografia1/src/criptografia1/Criptografia1.java
+++ criptografia1/src/criptografia1/Criptografia1.java
@@ -13,17 +13,13 @@ public class Criptografia1 {
 		ArrayList<String> alfabeto = new ArrayList<String>();
 
 		alfabeto.add(" ");
-		for (int i = 65; i < 91; i++) {
 
-			char a = (char) i;
-			alfabeto.add(String.valueOf(a));
-		}
 		for (int i = 97; i < 123; i++) {
 
 			char a = (char) i;
 			alfabeto.add(String.valueOf(a));
 		}
-
+		alfabeto.add("ç");
 		return alfabeto;
 
 	}
@@ -35,40 +31,36 @@ public class Criptografia1 {
 
 		String mensagem = s.nextLine();
 
-		//tamanho da mensagem
+		// tamanho da mensagem
 		int tamanho = mensagem.length();
-		//calcular o resto da divisão por 5
-		int resto = tamanho%5;
-		//se não for exato, calcula quanto somar para adicionar 0
+		// calcular o resto da divisão por 5
+		int resto = tamanho % 5;
+		// se não for exato, calcula quanto somar para adicionar 0
 		int somar = 0;
-		if (resto !=0) {
-			somar = 5 - resto;			
+		if (resto != 0) {
+			somar = 5 - resto;
 		}
-		
+
 		int charTamanho = tamanho + somar;
-		char [] msgConvertida =  new char [ charTamanho ];
-		msgConvertida = mensagem.toCharArray();
-		
-		for (int i= tamanho+somar; i< charTamanho; i++) {
-			msgConvertida[i]=0;
+		char[] msgConvertida = new char[charTamanho];
+
+		mensagem.getChars(0, mensagem.length(), msgConvertida, 0);
+
+		for (int i = mensagem.length(); i < charTamanho; i++) {
+			msgConvertida[i] = 48;
 		}
-		
+
 		for (int i = 0; i < msgConvertida.length; i++) {
 
-			for (int j = 0; j < 5; j++) {
-				msgConvertida[i] += senha[j];
-			i++;
-			}
-			i++;
+			msgConvertida[i] += senha[i % 5];
 		}
-		
-		
+
 		System.out.println(resto);
 		System.out.println(somar);
 		System.out.println(msgConvertida);
 		System.out.println(msgConvertida.length);
 		System.out.println(charTamanho);
-//		System.out.println(msgConvertida[15]);
+//		System.out.println(msgConvertida[14]);
 
 		s.close();
 	}
