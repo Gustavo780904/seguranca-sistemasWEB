8798374b44e95161a2854d2ce7025482f1c24900
Gustavo780904 <gustavodutrasantos@gmail.com>
2022-02-28 11:29:46 -0300

    metodo para encontrar o index no alfabeto criado

--- criptografia1/src/criptografia1/Criptografia1.java
+++ criptografia1/src/criptografia1/Criptografia1.java
@@ -1,36 +1,42 @@
 package criptografia1;
 
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Criptografia1 {
 
 	static char[] senha = { 10, 8, 4, 11, 2 };
 
-	public static Map<Character, Integer> alfabeto() {
-		Map<Character, Integer> alfabeto = new HashMap<>();
-		alfabeto.put(' ', 0);
-		for (int i = 0; i < 26; i++) {
+	public static char[] alfabeto() {
 
-			char a = (char) ((char) i + 97);
-			alfabeto.put(a, i + 1);
+		char[] alfabeto = new char[28];
+		alfabeto[0] = ' ';
+		for (int i = 0; i < 26; i++) {
+			alfabeto[i + 1] = (char) (i + 97);
 		}
-		alfabeto.put('ç', 27);
+		alfabeto[27] = 'ç';
 
 		return alfabeto;
 	}
 
+	private static int findIndex(char alfabeto[], char elemento) {
+		int tamanho = alfabeto.length;
+		for (int i = 0; i < tamanho; i++) {
+			if (alfabeto[i] == elemento) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner s = new Scanner(System.in);
-		
-		
-		//String mensagem = s.nextLine();
-		String mensagem = "que a força esteja com voce";
-		
+
+		 String mensagem = s.nextLine();
+//		String mensagem = "que a força esteja com voce";
+
 		// tamanho da mensagem
 		int tamanho = mensagem.length();
 		// calcular o resto da divisão por 5
@@ -40,30 +46,25 @@ public class Criptografia1 {
 		if (resto != 0) {
 			somar = 5 - resto;
 		}
-		Map<Character, Integer> alfabeto = alfabeto();
+		char[] alfabeto = alfabeto();
 
 		int charTamanho = tamanho + somar;
 		char[] msgConvertida = new char[charTamanho];
 
 		mensagem.getChars(0, mensagem.length(), msgConvertida, 0);
-		
 
 		for (int i = mensagem.length(); i < charTamanho; i++) {
 			msgConvertida[i] = 48;
 		}
 
 		for (int i = 0; i < msgConvertida.length; i++) {
-			var index = alfabeto.get(msgConvertida[i]);
-			msgConvertida[i] += senha[i % 5];
+			int index = findIndex(alfabeto, msgConvertida[i]) + senha[i % 5];
+			//soma a partir da primeira posição quando exceder as posições do alfabeto
+			index = index % 28;
+			msgConvertida[i] = alfabeto[index];
 		}
 
-		System.out.println(resto);
-		System.out.println(somar);
 		System.out.println(msgConvertida);
-		System.out.println(msgConvertida.length);
-		System.out.println(alfabeto());
-		System.out.println(charTamanho);
-		System.out.println(msgConvertida[14]);
 
 		s.close();
 	}
