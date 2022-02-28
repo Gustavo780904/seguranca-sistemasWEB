0f2bf27ef0c958d057753a0041cac6575c752367
Gustavo780904 <gustavodutrasantos@gmail.com>
2022-02-28 10:48:22 -0300

    tentativa com hashmap

--- criptografia1/src/criptografia1/Criptografia1.java
+++ criptografia1/src/criptografia1/Criptografia1.java
@@ -1,36 +1,36 @@
 package criptografia1;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Criptografia1 {
 
 	static char[] senha = { 10, 8, 4, 11, 2 };
 
-	// alfabeto
-	public static ArrayList<String> alfabeto() {
-		ArrayList<String> alfabeto = new ArrayList<String>();
+	public static Map<Character, Integer> alfabeto() {
+		Map<Character, Integer> alfabeto = new HashMap<>();
+		alfabeto.put(' ', 0);
+		for (int i = 0; i < 26; i++) {
 
-		alfabeto.add(" ");
-
-		for (int i = 97; i < 123; i++) {
-
-			char a = (char) i;
-			alfabeto.add(String.valueOf(a));
+			char a = (char) ((char) i + 97);
+			alfabeto.put(a, i + 1);
 		}
-		alfabeto.add("ç");
-		return alfabeto;
+		alfabeto.put('ç', 27);
 
+		return alfabeto;
 	}
 
 	public static void main(String[] args) {
-
+		
 		Locale.setDefault(Locale.US);
 		Scanner s = new Scanner(System.in);
-
-		String mensagem = s.nextLine();
-
+		
+		
+		//String mensagem = s.nextLine();
+		String mensagem = "que a força esteja com voce";
+		
 		// tamanho da mensagem
 		int tamanho = mensagem.length();
 		// calcular o resto da divisão por 5
@@ -40,18 +40,20 @@ public class Criptografia1 {
 		if (resto != 0) {
 			somar = 5 - resto;
 		}
+		Map<Character, Integer> alfabeto = alfabeto();
 
 		int charTamanho = tamanho + somar;
 		char[] msgConvertida = new char[charTamanho];
 
 		mensagem.getChars(0, mensagem.length(), msgConvertida, 0);
+		
 
 		for (int i = mensagem.length(); i < charTamanho; i++) {
 			msgConvertida[i] = 48;
 		}
 
 		for (int i = 0; i < msgConvertida.length; i++) {
-
+			var index = alfabeto.get(msgConvertida[i]);
 			msgConvertida[i] += senha[i % 5];
 		}
 
@@ -59,8 +61,9 @@ public class Criptografia1 {
 		System.out.println(somar);
 		System.out.println(msgConvertida);
 		System.out.println(msgConvertida.length);
+		System.out.println(alfabeto());
 		System.out.println(charTamanho);
-//		System.out.println(msgConvertida[14]);
+		System.out.println(msgConvertida[14]);
 
 		s.close();
 	}
